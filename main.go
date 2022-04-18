41170635f03ef481ce436bce064174ee7a396e07
SodaCris <wang-jiaxun@outlook.com>
2022-04-18 09:33:14 +0800

    add test

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 // #include "myheader.h"
 import "C"
-import "unsafe"
+import (
+	"math"
+	"unsafe"
+)
+
+func Abs(t int) int {
+	return int(math.Abs(float64(t)))
+}
 
 func main() {
 	cs := C.CString("Hello from stdio")
 	C.myprint(cs)
+	println(Abs(-22))
 	C.free(unsafe.Pointer(cs))
 }
